6c7f73b648cecca04de5499295d093fe7173977d
Steen F. Tøttrup <sft@creativeminds.dk>
2018-11-19 13:42:32 +0100

    Second test added

--- test/CreativeMinds.Identity.MongoDBStores.Tests/UnitTest1.cs
+++ test/CreativeMinds.Identity.MongoDBStores.Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using MongoDB.Driver;
 using System;
+using System.Linq;
 using Xunit;
 
 namespace CreativeMinds.Identity.MongoDBStores.Tests {
@@ -8,18 +9,39 @@ namespace CreativeMinds.Identity.MongoDBStores.Tests {
 	public class UnitTest1 {
 		private const String mongoUrl = "mongodb://localhost/test";
 
-		[Fact]
-		public void Test1() {
+		private static UserStore<MockUser, MockRole, MockUserToken> GetUserStore() {
 			MongoUrl url = new MongoUrl(mongoUrl);
 			IMongoClient client = new MongoClient(url);
 			IMongoDatabase database = client.GetDatabase(url.DatabaseName);
 			IMongoCollection<MockUser> users = database.GetCollection<MockUser>("user");
 			IMongoCollection<MockRole> roles = database.GetCollection<MockRole>("role");
 
-			UserStore<MockUser, MockRole, MockUserToken> userStore = new UserStore<MockUser, MockRole, MockUserToken>(users, roles);
-			IdentityResult result = userStore.CreateAsync(new MockUser { Email = "test@test.com", UserName = "MisterTest", PasswordHash = "" }, new System.Threading.CancellationToken()).Result;
+			return new UserStore<MockUser, MockRole, MockUserToken>(users, roles);
+		}
+
+		[Fact]
+		public void CreateUser() {
+			UserStore<MockUser, MockRole, MockUserToken> userStore = GetUserStore();
+			IdentityResult result = userStore.CreateAsync(new MockUser { Email = "test1@test.com", UserName = "MisterTest", PasswordHash = "" }, new System.Threading.CancellationToken()).Result;
 
 			Assert.True(result.Succeeded == true);
+		}
+
+		[Fact]
+		public void FetchUser() {
+			UserStore<MockUser, MockRole, MockUserToken> userStore = GetUserStore();
+			String emailAddress = "test2@test.com";
+			var user = new MockUser { Email = emailAddress, UserName = "MisterTest", PasswordHash = "" };
+			IdentityResult result = userStore.CreateAsync(user, new System.Threading.CancellationToken()).Result;
+
+			Assert.True(result.Succeeded == true);
+
+			var users = userStore.Users.ToList();
+			var usr =  users.FirstOrDefault(u => u.Email == emailAddress);
+
+			user = userStore.FindByEmailAsync(usr.NormalizedEmail, new System.Threading.CancellationToken()).Result;
+
+			Assert.NotNull(user);
 		}
 	}
 }
