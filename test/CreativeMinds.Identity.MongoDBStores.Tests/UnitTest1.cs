48443e7f4859e23cec21a779195a3653fce17bbb
Steen F. Tøttrup <sft@creativeminds.dk>
2018-11-19 13:15:22 +0100

    Adding first test

---
+++ test/CreativeMinds.Identity.MongoDBStores.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Identity;
+using MongoDB.Driver;
+using System;
+using Xunit;
+
+namespace CreativeMinds.Identity.MongoDBStores.Tests {
+
+	public class UnitTest1 {
+		private const String mongoUrl = "mongodb://localhost/test";
+
+		[Fact]
+		public void Test1() {
+			MongoUrl url = new MongoUrl(mongoUrl);
+			IMongoClient client = new MongoClient(url);
+			IMongoDatabase database = client.GetDatabase(url.DatabaseName);
+			IMongoCollection<MockUser> users = database.GetCollection<MockUser>("user");
+			IMongoCollection<MockRole> roles = database.GetCollection<MockRole>("role");
+
+			UserStore<MockUser, MockRole, MockUserToken> userStore = new UserStore<MockUser, MockRole, MockUserToken>(users, roles);
+			IdentityResult result = userStore.CreateAsync(new MockUser { Email = "test@test.com", UserName = "MisterTest", PasswordHash = "" }, new System.Threading.CancellationToken()).Result;
+
+
+
+		}
+	}
+}
